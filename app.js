f1517795d23cb5ec8f0e662afc051e210698bfd3
Riyad <onlygobs0@gmail.com>
2025-04-13 10:16:15 +0600

    Project completed

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const navMenu = document.querySelector('.nav-menu')
-const overlay = document.querySelector('.overlay')
-const openBtn = document.querySelector('.open-menu')
-const closeBtn = document.querySelector('.close-menu')
+const navMenu = document.querySelector('.nav-menu');
+const overlay = document.querySelector('.overlay');
+const openBtn = document.querySelector('.open-menu');
+const closeBtn = document.querySelector('.close-menu');
 
-openBtn.addEventListener('click', toggleNavMenu)
-closeBtn.addEventListener('click', toggleNavMenu)
-overlay.addEventListener('click', toggleNavMenu)
+openBtn.addEventListener('click', toggleNavMenu);
+closeBtn.addEventListener('click', toggleNavMenu);
+overlay.addEventListener('click', toggleNavMenu);
 
 function toggleNavMenu() {
   navMenu.classList.toggle('appear');
   overlay.classList.toggle('appear');
-}
+};
